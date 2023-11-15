bcdc0ffc67ffb9a388179443e668792d5e7b3ef1
zhangshiyu <zhangshiyu9779@gmail.com>
2023-11-15 16:28:13 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -441,6 +441,38 @@ fn main() {
     let s2 = String::from("world");
     let s3 = format!("{}{}{}", s1, s2, "!"); //format!宏会返回一个String对象
     println!("s3 = {}", s3);
+    //字符串转义：可以通过'\'输出 ASCII 和 Unicode 字符
+    let s = "hello world \x52\x75\x73\x74\n";
+    println!("s = {}", s);
+    // \u 可以输出一个 unicode 字符
+    let unicode_codepoint = "\u{211D}";
+    let character_name = "\"DOUBLE-STRUCK CAPITAL R\"";
+
+    println!(
+        "Unicode character {} (U+211D) is called {}",
+        unicode_codepoint, character_name
+    );
+
+    // 换行了也会保持之前的字符串格式
+    // 使用\忽略换行符
+    let long_string = "String literals
+                        can span multiple lines.
+                        The linebreak and indentation here ->\
+                        <- can be escaped too!";
+    println!("{}", long_string);
+
+    println!("{}", "hello \\x52\\x75\\x73\\x74");
+    // 原始字符串 不转义
+    let raw_str = r"Escapes don't work here: \x3F \u{211D}";
+    println!("{}", raw_str);
+
+    // 如果字符串包含双引号，可以在开头和结尾加 #
+    let quotes = r#"And then I said: "There is no escape!""#;
+    println!("{}", quotes);
+
+    // 如果还是有歧义，可以继续增加，没有限制
+    let longer_delimiter = r###"A string with "# in it. And even "##!"###;
+    println!("{}", longer_delimiter);
 
 }
 
